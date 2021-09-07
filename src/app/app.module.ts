99333e0ac6099a899b0767784b9cc8dc6eace24e
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-09-07 16:38:44 -0300

    Adicionando tabela do product-read component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table';
 
 import { FormsModule } from '@angular/forms'
 
@@ -52,7 +53,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
